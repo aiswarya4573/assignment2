081df034fd5d5b22bc6cd1b17db3662b2a9f511c
aiswarya4573 <aiswarya457373@gmail.com>
2024-09-02 15:10:28 +0530

    uet

---
+++ index.js
@@ -0,0 +1,21 @@
+window.uetq = window.uetq || [];
+window.uetq.push('consent', 'default', {
+    'ad_storage': 'denied'
+    });
+
+
+
+
+  document.addEventListener("cookieyes_consent_update", function (eventData)
+ {
+   const data = eventData.detail;
+   if (data.accepted.includes("analytics")) {
+      // perform desired action.
+      window.uetq = window.uetq || [];
+        window.uetq.push('consent', 'default', {
+          'ad_storage': 'denied'
+            });
+
+    }
+  });
+ 
